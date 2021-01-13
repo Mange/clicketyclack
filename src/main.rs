ce37d96e5249229909a734132c71ea71d4377e5e
Magnus Bergmark <me@mange.dev>
2021-01-13 23:28:36 +0100

    Allow runtime setting of volume and model

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,48 @@ use crate::adapter::Adapter;
 use audio_player::AudioPlayer;
 use std::sync::mpsc;
 use std::thread;
+use structopt::clap::arg_enum;
+use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(Copy, Clone, Debug)]
+    pub enum Model {
+        KailhBoxWhite,
+    }
+}
+
+#[derive(StructOpt)]
+#[structopt(
+    about = "Play sounds of mechanical keyboards when you type, for that extra clicky feeling inside your headset without waking up your family in the night.",
+    setting = structopt::clap::AppSettings::ColoredHelp
+)]
+struct Options {
+    #[structopt(
+        long = "model",
+        short = "m",
+        default_value = "KailhBoxWhite",
+        possible_values = &Model::variants(),
+        value_name = "NAME",
+    )]
+    model: Model,
+
+    #[structopt(
+        long = "volume",
+        short = "l",
+        alias = "level",
+        default_value = "1.0",
+        value_name = "FLOAT"
+    )]
+    volume: f32,
+}
 
 pub enum Event {
     KeyDown,
     KeyUp,
 }
 
-#[derive(Copy, Clone, Debug)]
-pub enum Model {
-    KailhBoxWhite,
-}
-
 fn main() -> Result<(), String> {
+    let options = Options::from_args();
     let (sender, receiver) = mpsc::channel();
 
     let adapter = Adapter::connect(sender).map_err(|error_code| {
@@ -31,9 +61,8 @@ fn main() -> Result<(), String> {
     });
 
     // Audio setup
-    let mut sounds = sounds::Sounds::load(Model::KailhBoxWhite);
-    let volume = 0.3; // TODO: Make this changeable at runtime somehow
-    let audio_player = AudioPlayer::new(volume);
+    let mut sounds = sounds::Sounds::load(options.model);
+    let audio_player = AudioPlayer::new(options.volume);
 
     for event in receiver {
         match event {
